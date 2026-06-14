f0fa2cd8c5d83f5f151da1b037a678b5786826e4
Sina Salimirad <SinaSalimirad@proton.me>
2026-06-14 19:24:43 +0330

    feat(auth): implement authentication endpoints and validation (#9)

--- apps/web/src/app/(auth)/hooks/use-auth.ts
+++ apps/web/src/app/(auth)/hooks/use-auth.ts
@@ -1,4 +1,5 @@
-import { useQuery } from "@tanstack/react-query";
+import type { SigninInput, SignupInput } from "@cockatiel/shared/schemas/auth/auth.schema";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import api from "@/lib/api";
 
 export function useCheckUsernameQuery(usernameQuery: string) {
@@ -19,4 +20,42 @@ export function useCheckUsernameQuery(usernameQuery: string) {
   });
 
   return { data, isLoading, isError };
+}
+
+export function useSigninMutate() {
+  const { mutate, isPending, isError } = useMutation({
+    mutationKey: ["auth", "sign-in"],
+    mutationFn: async (payload: SigninInput) => {
+      const response = await api.post("/v1/auth/sign-in", payload);
+
+      return response.data;
+    },
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: () => {
+      console.error(`something went wrong`);
+    },
+  });
+
+  return { mutate, isPending, isError };
+}
+
+export function useSignupMutate() {
+  const { mutate, isPending, isError } = useMutation({
+    mutationKey: ["auth", "sign-up"],
+    mutationFn: async (payload: SignupInput) => {
+      const response = await api.post("/v1/auth/sign-up", payload);
+
+      return response.data;
+    },
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: () => {
+      console.error(`something went wrong`);
+    },
+  });
+
+  return { mutate, isPending, isError };
 }
